6558be6a2c06a262c8ee083df2f7f85ecfa61738
PoProstuMichal89 <michumazur89@gmail.com>
2021-03-14 21:22:36 +0100

    Add fileds in class employee, add constructor for class employee, add method <IfWorkInDepartment> for chec if worker working in provided departmen

--- src/employeeManagment/employee.java
+++ src/employeeManagment/employee.java
@@ -2,6 +2,26 @@ package employeeManagment;
 
 public class employee {
 
+    private String name;
+    private String surname;
+    private double salary;
+    private char gender;
+    private int department;
 
+    public employee(String name, String surname, double salary, char gender, int department) {
+        this.name = name;
+        this.surname = surname;
+        this.salary = salary;
+        this.gender = gender;
+        this.department = department;
+    }
+    
 
+    public boolean IfWorkInDepartment(int department) {
+        if (this.department == department) {
+
+        }
+        return true;
+
+    }
 }
